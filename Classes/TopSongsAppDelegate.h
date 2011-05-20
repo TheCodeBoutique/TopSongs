c6f0fa6f4f4f43ab56daeb40ffa3f5bffaab3fb6
Chad Eubanks <ceubanks@thecodeboutique.com>
2011-05-19 22:45:51 -0700

    xml.... oneatage

---
+++ Classes/TopSongsAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TopSongsAppDelegate.h
+//  TopSongs
+//
+//  Created by Chad Eubanks on 5/19/11.
+//  Copyright 2011 The Code Boutique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopSongsAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ RSSTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  RSSTableViewController.h
+//  TopSongs
+//
+//  Created by Chad Eubanks on 5/19/11.
+//  Copyright 2011 The Code Boutique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RSSTableViewController : UIViewController {
+
+}
+
+@end
